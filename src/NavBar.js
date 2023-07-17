21a4981be1dac36fe82c70cc3ee50fa4f442655d
KIPROTYCH <kiprotych@gmail.com>
2023-07-17 22:09:16 +0300

    First Commit

---
+++ src/NavBar.js
@@ -0,0 +1,13 @@
+function NavBar () {
+    return(
+        <div>
+            <nav >
+                <li>Home</li>
+                <li>About Us</li>
+                
+            </nav>
+        </div>
+    )
+}
+
+export default NavBar;
